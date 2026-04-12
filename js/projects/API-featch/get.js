9d04904c646bef5f537c5014da20805d2bc58aae
AYUSHIPATEL123 <ayushi.devloper.work@gmail.com>
2026-04-12 14:52:38 +0530

    add paginetion

--- js/projects/API-featch/get.js
+++ js/projects/API-featch/get.js
@@ -1,34 +1,97 @@
+let current_page = 1
+const page_items = 3
+let start_index = 0
+let end_index = page_items
+let tbody = document.getElementById('tbody')
+let loader = document.getElementById('loader')
+let url = "https://jsonplaceholder.typicode.com/users"
 
-async function getUsers(){
+function get_pageIndex(current_page){
+    
+    start_index = (current_page - 1) * page_items
+    end_index = start_index + page_items
 
-    url = "https://jsonplaceholder.typicode.com/users"
+}
 
-    let tbody = document.getElementById('tbody')
-    let loader = document.getElementById('loader')
-
+async function displayItems(t_body){
+    
+    get_pageIndex(current_page)
+    
     tbody.innerHTML = ''
     
-    loader.style.display = "block";
 
     try {
-        response = await fetch(url)
-
-        data = await response.json()
         
+        let response = await fetch(url)
+        let data = await response.json()
+        data = data.slice(start_index,end_index)
+        
+        if(data.length === 0){
+
+            errorMessage("No more pages available")
+            current_page--
+            return
+
+        }
+
         for (let d of data){
+
             tr = document.createElement('tr')
 
             tr.innerHTML = `<td>${d.name}</td> <td>${d.email}</td> <td>${d.phone}</td>`
 
-            tbody.appendChild(tr)
+            t_body.appendChild(tr)
         }
+
     } catch (error) {
         
-        tr = document.createElement('tr')
-        tr.innerHTML = `${error}`
-        tbody.appendChild(tr)
+        errorMessage(error)
 
+    }   
+    document.getElementById('pageInfo').innerText = current_page
+}
+
+function errorMessage(msg){
+    
+    tbody.innerHTML = ''
+    let tr = document.createElement('tr')
+
+    tr.innerHTML = `
+        <td colspan="3">${msg}</td>
+    `
+
+    tbody.appendChild(tr)
+}
+
+async function previousPage(){
+    
+    if (current_page<=1){
+       errorMessage("page dose not exist.page can't be less than 1")
+       return
     }
+
+        current_page--
+        loader.style.display = "block";
+        await displayItems(tbody)
+        loader.style.display = "none";
+
+}
+
+async function nextPage(){
+
+    current_page++
+    loader.style.display = "block";
+    await displayItems(tbody)
+    loader.style.display = "none";
+
+}
+
+async function getUsers(){
+
+    loader.style.display = "block";
+
+    await displayItems(tbody)
+
     loader.style.display = "none";
 }
 
